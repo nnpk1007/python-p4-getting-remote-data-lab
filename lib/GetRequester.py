4c9328e707d2c65eecf6749c725d49f75618c9f6
Ngoc Nguyen <nnpk0410@gmail.com>
2023-09-01 15:51:11 -0500

    lab completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,24 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+
+        return response.content
 
     def load_json(self):
-        pass
+        json_content = []
+        json_data =  json.loads(self.get_response_body())
+
+        for data in json_data:
+            json_content.append(data)
+        
+        return json_content 
+
+
+custom_url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+responses = GetRequester(custom_url)
+
+# Fetch and process data using the instance
+json_content = responses.load_json()
+
+print(json_content)
